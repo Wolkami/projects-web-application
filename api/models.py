21dbf4c01664ccc77bc5d0f229ee2a2284f349d5
Wolkami <set1393@yandex.ru>
2025-06-23 21:24:59 +0300

    Создана кастомная модель пользователей
    - Создана модель `CustomUser`
    - Выполнены миграции
    - Создан суперпользователь

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('student', 'Студент'),
+        ('teacher', 'Преподаватель'),
+        ('admin', 'Администратор'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
+    group = models.CharField(max_length=20, blank=True, null=True)
